2d259d8454126d5759c2d74ad5b1ee40aa0e1086
Bhasker23 <shrimalibhaskar2398@gmail.com>
2022-09-13 16:48:22 +0530

    Day1 all question pushed

--- Day1 Assignment/src/com/Question1/InsideCircle.java
+++ Day1 Assignment/src/com/Question1/InsideCircle.java
@@ -1,5 +1,27 @@
 package com.Question1;
 
+import java.util.Scanner;
+
 public class InsideCircle {
 
+	public static void main(String[] args) {
+		
+		int r = 10;
+		
+		Scanner scanner = new  Scanner(System.in);
+		
+		System.out.println("Enter the point x ");
+		int x  = scanner.nextInt();
+		
+		System.out.println("Enter the point y ");
+		int y = scanner.nextInt();
+		
+		if (((x*x) + (y*y)) < (r*r)) {
+			
+			System.out.println("Inside the Circle ");
+		} else {
+			System.out.println("Outside the Circle ");
+		}
+		
+	}
 }
--- Day1 Assignment/src/com/Question2/Lottery.java
+++ Day1 Assignment/src/com/Question2/Lottery.java
@@ -1,5 +1,58 @@
 package com.Question2;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+
 public class Lottery {
 
+	public static void main(String[] args) {
+		
+		Random rd = new Random();
+		
+		int number = rd.nextInt(100,999);
+
+		
+		List<String> num = Arrays.asList(Integer.toString(number).trim().split(""));
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter your Lottery Number..");
+		String string = sc.next();
+
+		
+		List<String> lNum = Arrays.asList(string.trim().split(""));
+		
+		
+		
+		if(num.equals(lNum)) {
+			System.out.println("You have won 10,000 $ Money in Lottery ");
+		}
+		else {
+			
+			int count = 0;
+			
+			for(int i = 0; i<num.size(); i++) {
+//				System.out.println(num.get(i) +" "+ (lNum.get(i)));
+				if (num.contains(lNum.get(i))) {
+					
+					count++;
+				}
+			}
+			
+			if (count == 3) {
+				System.out.println("You won 3000 $ in Lottery..");
+			}
+			else if (count == 1) {
+				System.out.println("You won 1000 $ in Lottery..");
+			}
+			else {
+				System.out.println("oh sorry..Better luck next time..");
+			}
+			
+		}
+		
+//		System.out.println(num+" "+lNum);
+	}
 }
